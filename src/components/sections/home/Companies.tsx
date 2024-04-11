75cf629be631b119df30e663dd4d974786a29d1a
kiritoEM <johankirito64@gmail.com>
2024-04-11 07:47:34 +0200

    add buttons to hero

---
+++ src/components/sections/home/Companies.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Companies = (): JSX.Element => {
+  return (
+    <section id="home__companies">
+      <div className="companies-container">
+        <div className="sub-container">
+          <p>Trusted By 250+ Companies</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Companies;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/layouts/Layout";
 import MetaTitle from "@/components/MetaTitle";
+import Companies from "@/components/sections/home/Companies";
 import Hero from "@/components/sections/home/Hero";
 import constants from "@/helpers/constants";
 import { Fragment } from "react";
@@ -12,6 +13,7 @@ const home = (): JSX.Element => {
       <section id="home">
         <Layout>
           <Hero />
+          <Companies />
         </Layout>
       </section>
     </Fragment>
