ccd865320f202c7ea7723380c7795ef2f8555cbf
kiritoEM <johankirito64@gmail.com>
2024-04-11 07:21:36 +0200

    add buttons to hero

--- src/components/sections/home/Hero.tsx
+++ src/components/sections/home/Hero.tsx
@@ -1,6 +1,22 @@
 const Hero = (): JSX.Element => {
   return (
     <section id="home__hero">
+      <div className="hero-container">
+        <div className="title">
+          <h1>A Digital Product Studio that will Work</h1>
+        </div>
+        <div className="for-buttons">
+          <p>
+            For <span className="span-active">Startups</span>,{"     "}
+            <span className="span-active">Media & Publishers</span>and{"     "}
+            <span className="span-active">Social Good</span>
+          </p>
+        </div>
+        <div className="footer-buttons">
+          <button className="btn">Our works</button>
+          <button className="btn">Contact us</button>
+        </div>
+      </div>
       <div className="abstract-design">
         <img src="/abstract-design.png" alt="" />
       </div>
